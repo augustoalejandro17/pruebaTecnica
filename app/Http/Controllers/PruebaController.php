12d07c881994d6234931af764f2b1047fb19b7cc
Augusto Vaca <avaca@libelulasoft.com>
2023-12-05 20:55:22 -0500

    Añadir métodos para obtener valores

--- app/Http/Controllers/PruebaController.php
+++ app/Http/Controllers/PruebaController.php
@@ -9,30 +9,81 @@ class PruebaController extends Controller
     CONST FIRST_RANGE = 0.01;
     CONST SECOND_RANGE = 0.05;
     CONST THIRD_RANGE = 0.1;
+    CONST MIN_DAYS_WORKED = 30;
+    CONST USER = 'admin@admin.com';
+    CONST PASSWORD = 'Clave123+';
 
     public function index(Request $request)
     {
-        $user = $request->usuario;
-        $password = $request->contraseña;
-        $salary = (double)$request->salario_base;
-        $worked_days = (int)$request->dias_trabajados;
-        $sales_value = (int)$request->valor_ventas;
+        try {
+            $user = $request->usuario;
+            $password = $request->contraseña;
+            $salary = (double)$request->salario_base;
+            $worked_days = (int)$request->dias_trabajados;
+            $sales_value = (int)$request->valor_ventas;
+            $recalculated_percentage = 0;
+            $salary_recalculated = $salary;
 
-        $additionalValue = 0;
+            $this->checkCredentials($user, $password);
 
-        if($salary <= 0 || $worked_days <= 0 || $sales_value <= 0 ){
-            return response()->json("Uno de los valores ingresados es incorrecto", 202);
+            if($salary <= 0 || $worked_days <= 0 || $sales_value <= 0 ){
+                return response()->json("Uno de los valores ingresados es incorrecto", 202);
+            }
+
+            if($worked_days < self::MIN_DAYS_WORKED) {
+                $salary_recalculated = $this->getNewSalary($salary, $worked_days);
+                $recalculated_percentage = $this->getPercentage($worked_days);
+            }
+
+            $additional_value = $this->getAdditionValue($salary_recalculated, $sales_value);
+
+            $salary_response = (double)$salary_recalculated + (double)$additional_value;
+
+            $response = [
+                'salario_base' => $salary,
+                'dias_trabajados' => $worked_days,
+                'valor_ventas' => $sales_value,
+                'salario_calculado' => $salary_response,
+                'porcentage_prorrateo' => $recalculated_percentage
+            ];
+
+            return response()->json($response, 200);
+        } catch (\Throwable $th) {
+            $error_message = $th->getMessage();
+            return response()->json(['error' => $error_message], $th->getCode());
         }
+    }
 
+    public function checkCredentials($user, $password) 
+    {
+        if(!$user || !$password) {
+            throw new \Exception("Ingresar las credenciales", 202);
+        }
+        if(!($user == self::USER && $password = self::PASSWORD)) {
+            throw new \Exception("Las credenciales son incorrectas", 202);
+        }
+    }
+
+    public function getAdditionValue($salary, $sales_value) 
+    {
         if($sales_value <= 1000) {
-            $additionalValue = $salary *  self::FIRST_RANGE;
+            return $salary *  self::FIRST_RANGE;
         } elseif($sales_value <= 5000) {
-            $additionalValue = $salary *  self::SECOND_RANGE;
+            return $salary *  self::SECOND_RANGE;
         } elseif($sales_value > 5000){
-            $additionalValue = $salary *  self::THIRD_RANGE;
+            return $salary *  self::THIRD_RANGE;
         } else {
-            return response()->json("El valor del salario es incorreco", 202);
+            throw new \Exception("El valor del salario es incorrecto", 202);
         }
-        return response()->json($additionalValue);
+    }
+
+    public function getNewSalary($salary, $worked_days)
+    {
+        return ($salary*$worked_days)/self::MIN_DAYS_WORKED;
+    }
+
+    public function getPercentage($worked_days)
+    {
+        return 100 - ((100*$worked_days)/self::MIN_DAYS_WORKED);
     }
 }
